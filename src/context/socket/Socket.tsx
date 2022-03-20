90c83121207053575e2ea31b239456d8c1ac360e
Emerson Valentim <emevalentim0@gmail.com>
2022-03-19 21:38:55 -0300

    feat: add signin event to socket context

--- src/context/socket/Socket.tsx
+++ src/context/socket/Socket.tsx
@@ -1,23 +1,30 @@
 import React, { useContext, useEffect } from "react";
+import { Socket } from "socket.io-client";
 
 import useSocket from "../../hooks/use-socket";
 import UserContext from "../user/User";
 
-const defaultValue = {
-  socket: useSocket("socket-1"),
-};
-
-const SocketContext = React.createContext(defaultValue);
+const SocketContext = React.createContext({
+  socket: undefined as any as Socket,
+});
 
 SocketContext.displayName = "Socket";
 
 const SocketContextProvider: React.FC = ({ children }) => {
+  const defaultValue = {
+    socket: useSocket("socket-1"),
+  };
+
   const { isAuthenticated } = useContext(UserContext);
 
+  const establishConnection = () => {
+    defaultValue.socket.connect();
+
+    defaultValue.socket.emit("client:signIn");
+  };
+
   useEffect(() => {
-    isAuthenticated
-      ? defaultValue.socket.connect()
-      : defaultValue.socket?.disconnect();
+    isAuthenticated ? establishConnection() : defaultValue.socket?.disconnect();
   }, [isAuthenticated]);
 
   return (
