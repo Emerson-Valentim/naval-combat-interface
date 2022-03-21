0ca832afd5412d469ee7639c0e7697ef76f337e6
Emerson Valentim <emevalentim0@gmail.com>
2022-03-21 08:15:45 -0300

    feat: add profile data to user context

--- src/context/user/User.tsx
+++ src/context/user/User.tsx
@@ -1,16 +1,36 @@
+import { useLazyQuery } from "@apollo/client";
+import { gql } from "apollo-boost";
 import React, { useEffect, useState } from "react";
 
 import tokenStorage from "../../utils/token-storage";
 
+interface User {
+  id: string;
+  username: string;
+}
+
 const UserContext = React.createContext({
   isAuthenticated: false,
   setAuthentication: undefined as any,
+  user: undefined as any as User,
 });
 
 UserContext.displayName = "User";
 
+const PROFILE = gql`
+  query profile {
+    profile {
+      id
+      username
+    }
+  }
+`;
+
 const UserContextProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<User>();
   const [isAuthenticated, setAuthentication] = useState(false);
+
+  const [profile, { data }] = useLazyQuery(PROFILE);
 
   const authenticationState = tokenStorage.get();
 
@@ -18,8 +38,26 @@ const UserContextProvider: React.FC = ({ children }) => {
     setAuthentication(!!authenticationState);
   }, [authenticationState]);
 
+  useEffect(() => {
+    if (isAuthenticated) {
+      profile();
+    }
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (data) {
+      setUser(data.profile);
+    }
+  }, [data]);
+
   return (
-    <UserContext.Provider value={{ isAuthenticated, setAuthentication }}>
+    <UserContext.Provider
+      value={{
+        isAuthenticated,
+        setAuthentication,
+        user: user as User,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
