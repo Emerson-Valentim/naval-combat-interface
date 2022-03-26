ab1d54dba37846350c345c9c45fd33ada68ecd44
Emerson Valentim <emevalentim0@gmail.com>
2022-03-26 16:57:38 -0300

    refactor: remove logic from room

--- src/pages/Lobby/Room.tsx
+++ src/pages/Lobby/Room.tsx
@@ -1,51 +1,24 @@
-import { Text } from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
 
-import SocketContext from "../../context/socket/Socket";
-import UserContext from "../../context/user/User";
-import { parseBuffer } from "../../utils/buffer-parser";
+import Styled from "./styled";
+import Chat from "./Chat";
+import Info from "./Info";
 
 const Room: React.FC = () => {
-  const { user } = useContext(UserContext);
-  const { socket } = useContext(SocketContext);
-
-  const [message, addMessage] = useState<string[]>([]);
-
   const params = useParams();
 
-  useEffect(() => {
-    socket.on("client:room:connection", (message: any) => {
-      addMessage((value) => value.concat(`${message.username} entered room`));
-    });
-
-    socket.on("client:room:disconnect", (message: any) => {
-      addMessage((value) => value.concat(`${message.username} left room`));
-    });
-
-    return () => {
-      socket.off("client:room:connection");
-      socket.off("client:room:disconnect");
-
-      window.onbeforeunload = function () {
-        socket.emit(
-          "client:room:disconnect",
-          parseBuffer({
-            roomId: params,
-            username: user.username,
-          })
-        );
-      };
-    };
-  }, []);
-
-  return (
-    <>
-      {params.roomId}
-      {message.map((message, index) => (
-        <Text key={`message-${index}`}>{message}</Text>
-      ))}
-    </>
+  return params.roomId ? (
+    <Styled.RoomBox>
+      <Styled.RoomTitle>Sala - {params.roomId}</Styled.RoomTitle>
+      <>O jogo vem aqui</>
+      <Styled.RoomWidgets>
+        <Chat />
+        <Info roomId={params.roomId} />
+      </Styled.RoomWidgets>
+    </Styled.RoomBox>
+  ) : (
+    <Navigate to="/lobby" />
   );
 };
 
