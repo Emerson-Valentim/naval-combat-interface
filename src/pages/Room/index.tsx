925cfdf1795205d532aaba7adf49ff29fdd96d14
Emerson Valentim <emevalentim0@gmail.com>
2022-05-29 17:28:37 -0300

    refactor: update room structure

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -7,9 +7,31 @@ import FullscreenLoadingContext from "../../context/Loading";
 import SocketContext from "../../context/Socket";
 import UserContext from "../../context/User";
 
+import Board from "./components/Board";
 import Chat, { Messages } from "./components/Chat";
-import Info from "./components/Info";
+import LeaveRoomButton from "./components/LeaveRoomButton";
+import UserBoard from "./components/UserBoard";
+import UserInfo from "./components/UserInfo";
 import Styled from "./styled";
+
+interface Room {
+  id: string;
+  title: string;
+  players: string[];
+  board: Board;
+}
+
+export type TileStatus = "EMPTY" | "DESTROYED" | "MISSED" | "FILLED";
+export interface Board {
+  status: "DONE" | "PENDING";
+  currentPlayer: string;
+  size: number;
+  state: {
+    [userId: string]: {
+      positions: { [x: string]: TileStatus }[];
+    };
+  };
+}
 
 const GET_ROOM = gql`
   query getRoom($input: GetRoomInput!) {
@@ -18,6 +40,12 @@ const GET_ROOM = gql`
       title
       limit
       players
+      board {
+        status
+        currentPlayer
+        size
+        state
+      }
     }
   }
 `;
@@ -40,6 +68,15 @@ const Room: React.FC = () => {
     },
   });
 
+  const enemyId = data?.getRoom.players.find((enemyId: string) => {
+    if (user?.id) {
+      return enemyId !== user?.id;
+    }
+    return false;
+  });
+
+  console.log(enemyId);
+
   useEffect(() => {
     socket.on(
       "client:room:update",
@@ -61,11 +98,15 @@ const Room: React.FC = () => {
           updateMessages((value) =>
             value.concat([
               {
-                message: `${action === "leave" ? "left" : "entered"} the room`,
+                message: `${action === "leave" ? "Saiu" : "Entrou"} na sala`,
                 sender: username,
               },
             ])
           );
+        }
+
+        if (action === "turn") {
+          refetch();
         }
       }
     );
@@ -84,25 +125,39 @@ const Room: React.FC = () => {
   }, [loading]);
 
   return params.roomId ? (
-    <Styled.RoomBox>
-      <Styled.Room
-        height="100%"
-        backgroundImage={user.skin.current.scenario}
-        backgroundRepeat="no-repeat"
-        backgroundSize="cover"
-      >
-        <Styled.RoomTitle color="white">
-          Sala - {params.roomId}
-        </Styled.RoomTitle>
-      </Styled.Room>
-      <Styled.RoomWidgets>
-        <Chat messages={messages} />
-        {data ? <Info room={data?.getRoom} /> : null}
-      </Styled.RoomWidgets>
-    </Styled.RoomBox>
+    data && user ? (
+      <Styled.RoomBox>
+        <Styled.Room
+          height="100%"
+          backgroundImage={user?.skin.current.scenario}
+          backgroundRepeat="no-repeat"
+          backgroundSize="cover"
+        >
+          <Styled.FirstColumn>
+            <UserInfo user={user} />
+            <UserBoard board={data.getRoom.board} />
+            <LeaveRoomButton roomId={params.roomId} />
+          </Styled.FirstColumn>
+          <Styled.SecondColumn>
+            <Board board={data.getRoom.board} main userId={enemyId} />
+          </Styled.SecondColumn>
+          <Styled.ThirdColumn>
+            <UserInfo user={user} />
+            <Chat messages={messages} />
+          </Styled.ThirdColumn>
+        </Styled.Room>
+      </Styled.RoomBox>
+    ) : null
   ) : (
     <Navigate to="/lobby" />
   );
 };
+
+{
+  /* <Styled.RoomWidgets>
+        <Chat messages={messages} />
+        {data ? <Info room={data?.getRoom} /> : null}
+      </Styled.RoomWidgets> */
+}
 
 export default Room;
