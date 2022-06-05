1ee895b56b1faabfd0ec9c3a9c981ffedb1fdc83
Emerson Valentim <emevalentim0@gmail.com>
2022-06-05 17:55:54 -0300

    feat: add public profile query on user info

--- src/pages/Room/components/UserInfo/index.tsx
+++ src/pages/Room/components/UserInfo/index.tsx
@@ -1,29 +1,56 @@
+import { gql, useQuery } from "@apollo/client";
 import { Image, TagLabel } from "@chakra-ui/react";
 import React from "react";
 
-import { User } from "../../../../context/User";
-
 import Styled from "./styled";
 
-const UserInfo: React.FC<{ user: User }> = ({ user }) => {
-  const { wins, loses, matches } = user.meta;
+const GET_PUBLIC_PROFILE = gql`
+  query getPublicProfile($input: PublicProfileInput!) {
+    getPublicProfile(input: $input) {
+      id
+      username
+      meta {
+        wins
+        loses
+        matches
+      }
+      skin {
+        current {
+          avatar
+        }
+      }
+    }
+  }
+`;
+
+const UserInfo: React.FC<{ userId: string }> = ({ userId }) => {
+  const { data } = useQuery(GET_PUBLIC_PROFILE, {
+    variables: {
+      input: {
+        userId,
+      },
+    },
+  });
 
   return (
     <Styled.Container>
-      <Styled.Title>{user.username}</Styled.Title>
-      <Image src={user.skin.current.avatar} borderBottomRadius={10} />
+      <Styled.Title>{data?.getPublicProfile.username}</Styled.Title>
+      <Image
+        src={data?.getPublicProfile?.skin.current.avatar}
+        borderBottomRadius={10}
+      />
       <Styled.Meta>
         <Styled.MetaTag colorScheme="green">
           <TagLabel>Vitórias: </TagLabel>
-          {wins}
+          {data?.getPublicProfile?.meta.wins}
         </Styled.MetaTag>
         <Styled.MetaTag colorScheme="red">
           <TagLabel>Derrotas: </TagLabel>
-          {loses}
+          {data?.getPublicProfile?.meta.loses}
         </Styled.MetaTag>
         <Styled.MetaTag colorScheme="orange">
           <TagLabel>Partidas: </TagLabel>
-          {matches}
+          {data?.getPublicProfile?.meta.matches}
         </Styled.MetaTag>
       </Styled.Meta>
     </Styled.Container>
