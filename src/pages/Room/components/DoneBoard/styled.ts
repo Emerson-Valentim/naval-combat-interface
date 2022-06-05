908c75bcc3f9b608e389213b2462f1febb75633a
Emerson Valentim <emevalentim0@gmail.com>
2022-06-05 17:55:15 -0300

    feat: add done board

---
+++ src/pages/Room/components/DoneBoard/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Action = styled.div`
+  width: fit-content;
+
+  font-weight: 700;
+  background-color: white;
+
+  padding: 5px;
+  border-radius: 10px;
+  margin-top: 2em;
+`;
+
+export default {
+  Action,
+};
